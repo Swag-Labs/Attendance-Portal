183702d8005ae0cc800cbe6e5c72c915803e52f7
chiraggoyal0714 <chirag.goyal0714@gmail.com>
2017-09-07 16:55:34 +0530

    Modification in views

--- application/views/blog.php
+++ application/views/blog.php
@@ -1,4 +1,5 @@
 <p>
     Your attendance is already done
+     Please Relogin and logout to update your working hours
 </p>
 <b id="logout"><a href="logout">Logout</a></b>
--- application/views/final.php
+++ application/views/final.php
@@ -1,4 +1,5 @@
 <p>
     hey Attendance done
+    Please Relogin and logout to update your working hours
 </p>
 <b id="logout"><a href="logout">Logout</a></b>
